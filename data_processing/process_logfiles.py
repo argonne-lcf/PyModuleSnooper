6546c0baa4ea7b025f6dbdb6d5e78b99dd9d4447
Taylor Childers <taylorchilders@gmail.com>
2023-09-11 09:33:50 -0500

    adding exception reporting

--- data_processing/process_logfiles.py
+++ data_processing/process_logfiles.py
@@ -13,74 +13,78 @@ def extract_data_from_log(log_filename, ignore_modules, categories):
          print("failed to parse the json in file: ",log_filename)
          return None
 
-   rows = []
-   for module, version in log_data["versions"].items():
-      env = log_data["env"]
-      queue_name = "N/A"
-      account = "N/A"
-      nodenum = "N/A"
-      job_name = "N/A"
-      job_dir = "N/A"
-      pals_depth = env.get("PALS_DEPTH","N/A")
-      pals_local_rankid = env.get("PALS_LOCAL_RANKID","N/A")
-      pals_nodeid = env.get("PALS_NODEID","N/A")
-      pals_rankid = env.get("PALS_RANKID","N/A")
+   try:
+      rows = []
+      for module, version in log_data["versions"].items():
+         env = log_data["env"]
+         queue_name = "N/A"
+         account = "N/A"
+         nodenum = "N/A"
+         job_name = "N/A"
+         job_dir = "N/A"
+         pals_depth = env.get("PALS_DEPTH","N/A")
+         pals_local_rankid = env.get("PALS_LOCAL_RANKID","N/A")
+         pals_nodeid = env.get("PALS_NODEID","N/A")
+         pals_rankid = env.get("PALS_RANKID","N/A")
 
-      if "PBS_JOBID" in env:
-        job_id = env.get("PBS_JOBID", "N/A")  # Assuming the PBS_JOBID exists, else None is returned
-        queue_name = env.get("PBS_QUEUE", "N/A")
-        account = env.get("PBS_ACCOUNT","N/A")
-        nodenum = env.get("PBS_NODENUM","N/A")
-        job_name = env.get("PBS_JOBNAME","N/A")
-        job_dir = env.get("PBS_JOBDIR","N/A")
-      elif "COBALT_JOBID" in env:
-        job_id = env.get("COBALT_JOBID","N/A")
-        queue_name = env.get("COBALT_QUEUE", "N/A")
-        job_size = env.get("COBALT_PARTSIZE", "N/A")
-        account = env.get("COBALT_ACCOUNT","N/A")
-        job_name = env.get("COBALT_JOBNAME","N/A")
-      elif "login" in log_data["hostname"]:
-         # ignore login node runs
-         return None
-      
-      # PMI variables set by MPICH when running MPI
-      job_size = env.get("PMI_SIZE",env.get("WORLD_SIZE",env.get("NRANKS",env.get("NUMRANKS",1))))
-      job_id = env.get("PMI_JOBID","N/A")
-      pmi_local_rank = env.get("PMI_LOCAL_RANK","N/A")
-      pmi_local_size = env.get("PMI_LOCAL_SIZE","N/A")
-      pmi_rank = env.get("PMI_RANK","N/A")
-      pmi_size = env.get("PMI_SIZE","N/A")
-      
-      data_row = {
-         "Module": module,
-         "Version": version,
-         "User": env["USER"],
-         "Hostname": log_data["hostname"],
-         "Timestamp": log_data["timestamp"],
-         "Python Executable": log_data["sys.executable"],
-         "Ignored": module in ignore_modules,
-         "Job ID": job_id,
-         "Queue": queue_name,
-         "Job Size": job_size,
-         "Account": account,
-         "Node Number": nodenum,
-         "Job Name": job_name,
-         "Job Directory": job_dir,
-         "PALS Depth": pals_depth,
-         "PALS Rank ID": pals_rankid,
-         "PALS Local Rank ID": pals_local_rankid,
-         "PALS Node ID": pals_nodeid,
-         "PMI Local Rank": pmi_local_rank,
-         "PMI Local Size": pmi_local_size,
-         "PMI Rank": pmi_rank,
-         "PMI Size": pmi_size,
-      }
+         if "PBS_JOBID" in env:
+            job_id = env.get("PBS_JOBID", "N/A")  # Assuming the PBS_JOBID exists, else None is returned
+            queue_name = env.get("PBS_QUEUE", "N/A")
+            account = env.get("PBS_ACCOUNT","N/A")
+            nodenum = env.get("PBS_NODENUM","N/A")
+            job_name = env.get("PBS_JOBNAME","N/A")
+            job_dir = env.get("PBS_JOBDIR","N/A")
+         elif "COBALT_JOBID" in env:
+            job_id = env.get("COBALT_JOBID","N/A")
+            queue_name = env.get("COBALT_QUEUE", "N/A")
+            job_size = env.get("COBALT_PARTSIZE", "N/A")
+            account = env.get("COBALT_ACCOUNT","N/A")
+            job_name = env.get("COBALT_JOBNAME","N/A")
+         elif "login" in log_data["hostname"]:
+            # ignore login node runs
+            return None
+         
+         # PMI variables set by MPICH when running MPI
+         job_size = env.get("PMI_SIZE",env.get("WORLD_SIZE",env.get("NRANKS",env.get("NUMRANKS",1))))
+         job_id = env.get("PMI_JOBID","N/A")
+         pmi_local_rank = env.get("PMI_LOCAL_RANK","N/A")
+         pmi_local_size = env.get("PMI_LOCAL_SIZE","N/A")
+         pmi_rank = env.get("PMI_RANK","N/A")
+         pmi_size = env.get("PMI_SIZE","N/A")
+         # print("job id: ",job_id)
+         data_row = {
+            "Module": module,
+            "Version": version,
+            "User": env.get("USER",env.get('PBS_O_LOGNAME',str(env.get('HOME')).split('/'[-1]))),
+            "Hostname": log_data["hostname"],
+            "Timestamp": log_data["timestamp"],
+            "Python Executable": log_data["sys.executable"],
+            "Ignored": (module in ignore_modules) or module.startswith('_') or ('.' in module),
+            "Job ID": job_id,
+            "Queue": queue_name,
+            "Job Size": job_size,
+            "Account": account,
+            "Node Number": nodenum,
+            "Job Name": job_name,
+            "Job Directory": job_dir,
+            "PALS Depth": pals_depth,
+            "PALS Rank ID": pals_rankid,
+            "PALS Local Rank ID": pals_local_rankid,
+            "PALS Node ID": pals_nodeid,
+            "PMI Local Rank": pmi_local_rank,
+            "PMI Local Size": pmi_local_size,
+            "PMI Rank": pmi_rank,
+            "PMI Size": pmi_size,
+         }
 
-      data_row["Category"] = next((k for k, v in categories.items() if module in v), "none")
-      
-      rows.append(data_row)
+         data_row["Category"] = next((k for k, v in categories.items() if module in v), "none")
+         
+         rows.append(data_row)
 
-   return pd.DataFrame(rows)
+      return pd.DataFrame(rows)
+   except:
+      print('failed to parse: ',log_filename)
+      raise
 
 def parallel_processing(log_files, ignore_modules, categories, n_processes):
    with Pool(n_processes) as p:
