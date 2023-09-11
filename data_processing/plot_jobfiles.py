b318fcc1d98f2cdba8bfd776ee990b07c88ee018
Taylor Childers <taylorchilders@gmail.com>
2023-09-11 09:34:38 -0500

    many udpates

--- data_processing/plot_jobfiles.py
+++ data_processing/plot_jobfiles.py
@@ -116,8 +116,9 @@ def plot_accounts(df, filename, accounts_to_exclude=None, users_to_exclude=None)
       df = df[~df['User'].isin(users_to_exclude)]
 
    account_node_hours = df.groupby('Account')['Node-Hours'].sum()
+   total_node_hours = int(account_node_hours.sum())
    account_node_hours.plot(kind='bar', figsize=(12, 6))
-   plt.title('Node-Hours by Account')
+   plt.title(f'Node-Hours by Account; Total Node-Hours: {total_node_hours}')
    plt.ylabel('Node-Hours')
    plt.xlabel('Account')
    plt.yscale("log")
@@ -133,8 +134,11 @@ def plot_users(df, filename, accounts_to_exclude=None, users_to_exclude=None):
       df = df[~df['User'].isin(users_to_exclude)]
 
    user_node_hours = df.groupby('User')['Node-Hours'].sum()
+   print(user_node_hours)
+   user_node_hours = user_node_hours.sort_values(ascending=False)
+   total_node_hours = int(user_node_hours.sum())
    user_node_hours.head(20).plot(kind='bar', figsize=(12, 6))  # Display top 20 users for clarity
-   plt.title('Node-Hours by User')
+   plt.title(f'Node-Hours by User; Total Node-Hours: {total_node_hours}')
    plt.ylabel('Node-Hours')
    plt.xlabel('User')
    plt.yscale("log")
@@ -149,9 +153,10 @@ def plot_award_category(df, filename, accounts_to_exclude=None, users_to_exclude
       df = df[~df['User'].isin(users_to_exclude)]
       
    award_node_hours = df.groupby('Award Category')['Node-Hours'].sum()
+   total_node_hours = int(award_node_hours.sum())
    plt.figure(figsize=(10, 6))
    award_node_hours.plot.pie(autopct='%1.1f%%', startangle=90)
-   plt.title(f'Node-Hours by Award Category - Total Node-Hours: {award_node_hours.sum():.2f}')
+   plt.title(f'Node-Hours by Award Category - Total Node-Hours: {total_node_hours}')
    plt.ylabel('')
    plt.tight_layout()
    plt.savefig(filename)
@@ -237,9 +242,6 @@ def plot_module_usage_node_hours(df, filename, accounts_to_exclude=None, users_t
    if users_to_exclude:
       df = df[~df['User'].isin(users_to_exclude)]
 
-   # Calculate node-hours for each job
-   df['Node-Hours'] = df['Nodes'] * df['Runtime']
-
    # Filter the DataFrame for jobs that used either tensorflow or torch
    tensorflow_jobs = df[df['Non-Ignored Modules'].apply(lambda x: 'tensorflow' in x)]
    torch_jobs = df[df['Non-Ignored Modules'].apply(lambda x: 'torch' in x)]
@@ -247,6 +249,7 @@ def plot_module_usage_node_hours(df, filename, accounts_to_exclude=None, users_t
    # Calculate node-hours for each module
    tensorflow_node_hours = tensorflow_jobs['Node-Hours'].sum()
    torch_node_hours = torch_jobs['Node-Hours'].sum()
+   total_node_hours = int(tensorflow_node_hours + torch_node_hours)
 
    # Create data for the pie plot
    labels = ['TensorFlow', 'Torch']
@@ -255,11 +258,79 @@ def plot_module_usage_node_hours(df, filename, accounts_to_exclude=None, users_t
    # Plot
    plt.figure(figsize=(10, 6))
    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-   plt.title('Node-Hours by Module Usage')
+   plt.title(f'Node-Hours by Module Usage; Total Node-Hours: {total_node_hours}')
    plt.tight_layout()
    plt.savefig(filename)
    plt.close()
 
+
+def plot_module_usage_by_account(df, filename, accounts_to_exclude=None, users_to_exclude=None):
+   if accounts_to_exclude:
+      df = df[~df['Account'].isin(accounts_to_exclude)]
+   if users_to_exclude:
+      df = df[~df['User'].isin(users_to_exclude)]
+
+   # Create boolean columns for tensorflow and torch jobs
+   df['TensorFlow'] = df['Non-Ignored Modules'].apply(lambda x: 'tensorflow' in x)
+   df['Torch'] = df['Non-Ignored Modules'].apply(lambda x: 'torch' in x)
+
+   # Filter to only rows that used either tensorflow or torch
+   relevant_df = df[df['TensorFlow'] | df['Torch']]
+
+   # Multiply boolean columns by Node-Hours to get Node-Hours for each module
+   relevant_df['TensorFlow Node-Hours'] = relevant_df['TensorFlow'] * relevant_df['Node-Hours']
+   relevant_df['Torch Node-Hours'] = relevant_df['Torch'] * relevant_df['Node-Hours']
+
+   # Group by Account and sum the Node-Hours for TensorFlow and Torch
+   grouped = relevant_df.groupby('Account')[['TensorFlow Node-Hours', 'Torch Node-Hours']].sum()
+
+   # Sort by total Node-Hours and take top 10
+   grouped['Total Node-Hours'] = grouped['TensorFlow Node-Hours'] + grouped['Torch Node-Hours']
+   top_accounts = grouped.sort_values('Total Node-Hours', ascending=False).head(10)
+
+   total_node_hours = int(top_accounts['Total Node-Hours'].sum())
+   # Plot
+   plt.figure(figsize=(15, 6))
+   top_accounts[['TensorFlow Node-Hours', 'Torch Node-Hours']].plot(kind='bar', stacked=False, ax=plt.gca())
+   plt.title(f"Node-Hours by Module Usage per Account; Total Node-Hours: {total_node_hours}")
+   plt.ylabel('Node-Hours')
+   plt.yscale("log")
+   plt.tight_layout()
+   plt.savefig(filename)
+   plt.close()
+
+def plot_queue_node_hours(df, filename, accounts_to_exclude=None, users_to_exclude=None):
+   if accounts_to_exclude:
+      df = df[~df['Account'].isin(accounts_to_exclude)]
+   if users_to_exclude:
+      df = df[~df['User'].isin(users_to_exclude)]
+   
+   # Group by queue and sum the node-hours
+   queue_node_hours = df.groupby('Queue')['Node-Hours'].sum()
+   total_node_hours = queue_node_hours.sum()
+
+   # Determine which queues are less than 4% of the total and label them as "Others"
+   mask = queue_node_hours / total_node_hours < 0.04
+   others_data = queue_node_hours[mask]
+   queue_node_hours = queue_node_hours[~mask]
+   queue_node_hours['Others'] = others_data.sum()
+
+   # Prepare text box content for "Others"
+   others_text = "\n".join([f"{index.strip():>20}:  {value / total_node_hours * 100:6.1f}%" for index, value in others_data.sort_values(ascending=False).items()])
+
+   # Plot
+   plt.figure(figsize=(10, 6))
+   ax = queue_node_hours.plot.pie(autopct='%1.1f%%', startangle=90, pctdistance=0.85)
+   plt.title('Node-Hours by Queue')
+   plt.ylabel('')  # remove the default "Queue" ylabel for aesthetics
+   
+   # Add text box
+   props = dict(boxstyle='round, pad=0.5', facecolor='white', alpha=0.5)
+   ax.text(1., 0.5, 'Make-up of Others:\n' + others_text, transform=ax.transAxes, fontsize=9, verticalalignment='top', bbox=props,horizontalalignment='left')
+
+   plt.tight_layout()
+   plt.savefig(filename)
+   plt.close()
 
 
 if __name__ == "__main__":
@@ -272,7 +343,7 @@ if __name__ == "__main__":
    print(args.input_glob)
    for glob_str in args.input_glob:
       filelist = glob.glob(glob_str)
-      for file in filelist:
+      for file in sorted(filelist):
          print(f"Reading data from {file}...")
          all_data.append(pd.read_csv(file, compression='gzip', converters={'Categories': eval}, parse_dates=['Timestamp']))
    
@@ -283,7 +354,14 @@ if __name__ == "__main__":
    # print(df['Nodes'], df['Runtime'])
    df['Node-Hours'] = df['Nodes'] * df['Runtime']
 
-   df = df[df['Node-Hours'] > 0]
+   # df.to_csv('temp.csv')
+
+   # print(df['Queue'].unique())
+   # print(df[df['Queue'] == 'small'][['Job ID','Queue','Nodes','User','Runtime','Walltime','Exit Status','Job State']])
+   # df = df[df['Node-Hours'] > 0]
+   # print(df[['Job ID','Queue','Nodes','User','Runtime','Walltime','Exit Status','Job State']])
+
+   # print(df['Queue'].unique())
 
    accounts_to_exclude = None
    accounts_to_exclude = ['datascience']
@@ -299,7 +377,9 @@ if __name__ == "__main__":
    plot_runtime_to_walltime_ratio(df, f"{args.output_prefix}_runtime_walltime_ratio.png", accounts_to_exclude=accounts_to_exclude)
    plot_runtime_to_walltime_ratio_weighted(df, f"{args.output_prefix}_runtime_walltime_ratio_weighted.png", accounts_to_exclude=accounts_to_exclude)
    plot_module_usage_node_hours(df, f"{args.output_prefix}_module_usage_node_hours.png", accounts_to_exclude=accounts_to_exclude)
-
+   plot_module_usage_by_account(df,f"{args.output_prefix}_module_usage_by_account.png", accounts_to_exclude=accounts_to_exclude)
+   plot_queue_node_hours(df, f"{args.output_prefix}_queue_node_hours.png", accounts_to_exclude=accounts_to_exclude)
+   
 
    
    print("All plots generated successfully.")
